39441addabddde2b87ab8cb2f5b5941863d265ba
brin207 <82695757+brin207@users.noreply.github.com>
2022-06-15 10:14:41 +0800

    Add files via upload

--- Group/jscript.js
+++ Group/jscript.js
@@ -1,11 +1,20 @@
 function showModal(imgsrc, name, description){
 	let modal = document.getElementById("modal")
 	modal.style.display = "block";
-	modal.style.left = event.clientX + "px";
-	modal.style.top = event.clientY + "px";
+	modal.style.left = event.clientX+100 + "px";
+	if(event.clientY>=563){
+		modal.style.top = event.clientY-300 + "px";
+	}
+	else if(563>event.clientY && event.clientY >=320){
+		modal.style.top = event.clientY-200 + "px";
+	}
+	else if(event.clientY<320){
+		modal.style.top = event.clientY-50 + "px";
+	}
 	modal.innerHTML = "<div id='container'><img id='pic' src='"+imgsrc+"'/><p id='title'>"+name+"</p><p>"+description+"</p></div>";
 	}
 function closeModal(){
 	let modall = document.getElementById("modal");
-	modall.style.display = "none";
+	modall.removeAttribute("style");
+	modall.innerHTML = "";
 	}
